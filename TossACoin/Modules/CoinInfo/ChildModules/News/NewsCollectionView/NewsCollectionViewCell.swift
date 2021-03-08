43e0adfea57f5bce71e4cd1b62ce7527a71b9f61
sprh.t <sprh.tt@gmail.com>
2021-03-08 16:20:52 +0300

    Сделала подгрузку новостей

---
+++ TossACoin/Modules/CoinInfo/ChildModules/News/NewsCollectionView/NewsCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  NewsCollectionViewCell.swift
+//  TossACoin
+//
+//  Created by Софья Тимохина on 08.03.2021.
+//
+
+import Foundation
---
+++ TossACoin/Modules/CoinInfo/ChildModules/News/NewsCollectionView/NewsCollectionViewFlowLayout.swift
@@ -0,0 +1,8 @@
+//
+//  NewsCollectionViewFlowLayout.swift
+//  TossACoin
+//
+//  Created by Софья Тимохина on 08.03.2021.
+//
+
+import Foundation
